27f33e7772f45dd568182748a520d63ea4c522db
xidjex <xidjex@bk.ru>
2017-05-01 17:55:44 +0300

    delete test funtion

--- js/script.js
+++ js/script.js
@@ -77,17 +77,6 @@ function Palette() { //Конструктор палитры цветов
 		}
 	};
 
-	function test11(){
-		this.test0 = function(x, y) {
-			return x + y;
-		};
-		this.test1 = function(x, y) {
-			return x - y;
-		}
-	}
-var test2 = new test11();
-	console.log(new test11()["test0"](1,1));
-
 	var colorGrade = function(startValue, intensity,  type, fieldWidth, fieldHeight, incrementI, incrementJ) { // Создание градиента цвета
 		var x = 0;
 		switch (type){
